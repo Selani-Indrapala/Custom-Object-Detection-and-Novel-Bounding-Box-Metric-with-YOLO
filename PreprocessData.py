4ef3452603356ad80b3e5e4d27a06f424b75e6a8
Selani Indrapala <66544479+Selani-Indrapala@users.noreply.github.com>
2025-02-15 22:10:55 +0530

    Update PreprocessData.py

--- PreprocessData.py
+++ PreprocessData.py
@@ -101,7 +101,7 @@ class DatasetSplitter:
 class DogCatDatasetProcessor:
     def __init__(self, annotations_dir, images_dir, output_dir, class_mapping):
         self.converter = DatasetConverter(annotations_dir, images_dir, output_dir, class_mapping)
-        self.splitter = DatasetSplitter(os.path.join(output_dir, 'images'), output_dir)
+        self.splitter = DatasetSplitter(images_dir, output_dir)
 
     def process(self):
         self.converter.process_annotations()
