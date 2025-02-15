42dde5ec9a1d20cfb39f5a940fd7aa301830f034
Selani Indrapala <66544479+Selani-Indrapala@users.noreply.github.com>
2025-02-15 20:27:21 +0530

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -161,9 +161,17 @@ class ComputeLoss:
                 lbox += (1.0 - iou).mean()  # iou loss
 
                 # Compute Aspect Ratio Loss
-                par = pwh[:, 0] / (pwh[:, 1] + 1e-6)  # Predicted Aspect Ratio (Avoid division by zero)
-                tar = tbox[i][:, 2] / (tbox[i][:, 3] + 1e-6)  # Target Aspect Ratio
-                lar += torch.mean((par - tar) ** 2)  # MSE loss for aspect ratio
+                # par = pwh[:, 0] / (pwh[:, 1] + 1e-6)  # Predicted Aspect Ratio (Avoid division by zero)
+                # tar = tbox[i][:, 2] / (tbox[i][:, 3] + 1e-6)  # Target Aspect Ratio
+                # lar += torch.mean((par - tar) ** 2)  # MSE loss for aspect ratio
+
+                # Compute Center Alignment Loss
+                pcx = pxy[:, 0]  # Predicted center x-coordinate
+                pcy = pxy[:, 1]  # Predicted center y-coordinate
+                tcx = tbox[i][:, 0]  # Target center x-coordinate
+                tcy = tbox[i][:, 1]  # Target center y-coordinate                
+                # lcenter = torch.mean((pcx - tcx) ** 2 + (pcy - tcy) ** 2)  # MSE loss for center alignment
+                lar = torch.mean((pcx - tcx) ** 2 + (pcy - tcy) ** 2)  # MSE loss for center alignment
 
                 # Objectness
                 iou = iou.detach().clamp(0).type(tobj.dtype)
